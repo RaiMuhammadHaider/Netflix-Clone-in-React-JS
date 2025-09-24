f6ca01d10a3dacd4bd156f87630cd028aef177a2
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2025-09-24 09:22:27 -0700

    titleCards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './TitleCards.css'
+import Cards_data from '../../assets/cards/Cards_data'
+import cards_data from '../../assets/cards/Cards_data'
 const TitleCards = () => {
   return (
-    <div className='titlecards'>TitleCards</div>
+    <div className='titlecards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {
+          cards_data.map((card , index)=>(
+            <div className="card" key={index}>
+              <img src={card.image} alt={card.name} className='movie-img' />
+              <p className='move-name'>{card.name}</p>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
